8831f5c7547abb12d4dda119716a77649266c4ff
Ivan Andreevets <ivan_grozdny@mail.ru>
2023-04-27 10:18:13 +0300

    Create additional classes

---
+++ AuthorizationAPI/AuthorizationAPI.Application/ApplicationResult.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation.Results;
+
+namespace AuthorizationAPI.Application
+{
+    public class ApplicationResult<T> where T : class
+    {
+        public IList<string> Errors { get; }
+        public T? Value { get; internal set; }
+        public bool IsComplite => Errors.Count == 0;
+
+        public ApplicationResult(T? value = default, params string[] errors)
+        {
+            Value = value;
+            Errors = errors.ToList();
+        }
+
+        public ApplicationResult(ValidationResult validationResult, T? value = default)
+        {
+            Value = value;
+            Errors = validationResult.Errors.Select(x => x.ErrorMessage).ToList();
+        }
+    }
+}
---
+++ AuthorizationAPI/AuthorizationAPI.Application/StaticHelpers/Hacher.cs
@@ -0,0 +1,17 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace AuthorizationAPI.Application.StaticHelpers
+{
+    internal static class Hacher
+    {
+        public static string StringToHach(string message)
+        {
+            using (var hashAlg = MD5.Create())
+            {
+                byte[] hash = hashAlg.ComputeHash(Encoding.UTF8.GetBytes(message));
+                return Convert.ToBase64String(hash);
+            }
+        }
+    }
+}
